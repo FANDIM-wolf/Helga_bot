a2ba4654cd8e30e009b893a844bdc3e41bf21659
Mikhail Yolkin <63235817+FANDIM-wolf@users.noreply.github.com>
2022-01-08 18:22:12 +0300

    Add files via upload

---
+++ cake.py
@@ -0,0 +1,14 @@
+# cake class
+
+
+
+class Cake(object):
+    id = 0 
+    name = " "
+    recipe = ""
+    key_words =[]
+    power_for_process = 0
+    photo_name = " "
+    def print_data_about_cake(self):
+        print(self.id , self.name , "Cake ." )
+    
